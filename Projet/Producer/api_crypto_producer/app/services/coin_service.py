1683202019b40a987819fec61e9f4ff3c9e3e22f
plegulluche <plegulluche@gmail.com>
2024-01-19 17:01:59 +0100

    [ADD] adding a batching for the coin list data

--- Projet/Producer/api_crypto_producer/app/services/coin_service.py
+++ Projet/Producer/api_crypto_producer/app/services/coin_service.py
@@ -12,11 +12,14 @@ API_BASE_URL= os.getenv('API_BASE_URL')
 def get_coins_list_service():
     endpoint = f'/coins/list?x_cg_demo_api_key={API_KEY}'
     url = f"{API_BASE_URL}{endpoint}"
-    print("DEBUG URL :", url)
     response = requests.get(url)
     if response.status_code == 200:
         coins_data = response.json()
-        send_to_kafka('crypto_topic', coins_data)  # Kafka Producer Call
+        # Batching
+        batch_size = 100
+        for i in range(0, len(coins_data), batch_size):
+            batch = coins_data[i:i+batch_size]
+            send_to_kafka('crypto_topic', batch)  # Send each batch
         return coins_data, 200
     else:
         return {"error": "Failed to fetch data"}, response.status_code
